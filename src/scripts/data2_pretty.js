bd91d81027e4ea0ec7eb63ca20e474e8a81690f9
James Ming <james@geni.com>
2019-05-11 08:17:12 -0700

    Update

---
+++ src/scripts/data2_pretty.js
@@ -0,0 +1,133 @@
+export const data2 = {
+  "data": {
+    "elements": [
+      {
+        "data": {
+          "name": "rectangle border black",
+          "aspectratio": false,
+          "rotation": 0,
+          "shadow": 0,
+          "show": {
+            "top": true,
+            "right": true,
+            "bottom": true,
+            "left": true
+          },
+          "border-width": "1",
+          "border-style": "solid",
+          "mirror": 1
+        },
+        "collection": "shapefour",
+        "style": {
+          "element": {
+            "width": "128px",
+            "height": "96px",
+            "left": "0px",
+            "top": "0px",
+            "-webkit-transform": "rotate(0deg) scaleX(1)",
+            "-moz-transform": "rotate(0deg) scaleX(1)",
+            "-ms-transform": "rotate(0deg) scaleX(1)",
+            "-o-transform": "rotate(0deg) scaleX(1)",
+            "transform": "rotate(0deg) scaleX(1)",
+            "z-index": 1
+          },
+          "shape": {
+            "width": "100%",
+            "height": "100%",
+            "border-color": "#000000",
+            "border-radius": "0px",
+            "background-color": "transparent",
+            "opacity": 1
+          },
+          "resizeWrapper": {
+            "-webkit-transform": "rotate(0deg)",
+            "-moz-transform": "rotate(0deg)",
+            "-ms-transform": "rotate(0deg)",
+            "-o-transform": "rotate(0deg)",
+            "transform": "rotate(0deg)"
+          }
+        }
+      },
+      {
+        "data": {
+          "name": "rectangle border black",
+          "aspectratio": false,
+          "rotation": 0,
+          "shadow": 0,
+          "show": {
+            "top": true,
+            "right": true,
+            "bottom": true,
+            "left": true
+          },
+          "border-width": "1",
+          "border-style": "solid",
+          "mirror": 1
+        },
+        "collection": "shapefour",
+        "style": {
+          "element": {
+            "width": "128px",
+            "height": "96px",
+            "left": "1148px",
+            "top": "0px",
+            "-webkit-transform": "rotate(0deg) scaleX(1)",
+            "-moz-transform": "rotate(0deg) scaleX(1)",
+            "-ms-transform": "rotate(0deg) scaleX(1)",
+            "-o-transform": "rotate(0deg) scaleX(1)",
+            "transform": "rotate(0deg) scaleX(1)",
+            "z-index": 2
+          },
+          "shape": {
+            "width": "100%",
+            "height": "100%",
+            "border-color": "#000000",
+            "border-radius": "0px",
+            "background-color": "transparent",
+            "opacity": 1
+          },
+          "resizeWrapper": {
+            "-webkit-transform": "rotate(0deg)",
+            "-moz-transform": "rotate(0deg)",
+            "-ms-transform": "rotate(0deg)",
+            "-o-transform": "rotate(0deg)",
+            "transform": "rotate(0deg)"
+          }
+        }
+      }
+    ],
+    "panels": [],
+    "canvas": {
+      "curPaperShape": {
+        "pageSize": "letter",
+        "layout": "portrait",
+        "pageSizeCustom": {
+          "width": 300,
+          "height": 250
+        }
+      }
+    },
+    "os": {
+      "screen": "1680 x 1050",
+      "browser": "Chrome",
+      "browserVersion": "74.0.3729.131",
+      "browserMajorVersion": 74,
+      "mobile": false,
+      "os": "Mac OS X",
+      "osVersion": "10_14_1",
+      "cookies": true,
+      "flashVersion": "no check"
+    },
+    "guides": [],
+    "isTemplate": false,
+    "version": "v1-1-25-216",
+    "width": 1275,
+    "height": 1650,
+    "fileTitle": "Untitled"
+  },
+  "google_id": "105870981217629422585",
+  "width": 1275,
+  "height": 1650,
+  "fileId": "false",
+  "version": "v1-1-25-216"
+};
